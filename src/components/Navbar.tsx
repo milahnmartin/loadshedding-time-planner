4d0010faf5655787e4e4dd3a88aeeea35c339cd6
milahnmartin <milahnmartin.develop@gmail.com>
2022-09-26 19:06:20 +0200

    feat(ui): Added Button to Main

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className='relative w-screen h-10 flex place-items-center'>
+      <h1 className='font-mono font-extrabold text-white text-2xl tracking-widest'>
+        Eskom Gaming Calc
+      </h1>
+    </div>
+  );
+}
+
+export default Navbar;

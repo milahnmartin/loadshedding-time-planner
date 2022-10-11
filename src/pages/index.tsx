3002133ebd1ced4f7777d5127dd63b91a8b0e47a
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-11 18:46:59 +0200

    feat(seo): Added SEO For Main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,12 @@ const Home: NextPage = () => {
     <div className='h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 max-h-screen overflow-scroll'>
       <Head>
         <title>LS Time Planner / Home</title>
-        <meta name='description' content='this is Eskom Thingy' />
+        <meta
+          name='description'
+          content='Struggling To Schedule Gaming Sessions With your Friends ? Sign Up and Add your loadshedding Schedules and sit back and enjoy your stress free gaming planning.'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='image' content='/Light-buld.png' />
         <link rel='icon' href='/Light-bulb.png' />
         <link
           href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;1,100&display=swap'

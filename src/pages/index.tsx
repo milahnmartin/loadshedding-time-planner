227223c3299b539509b2208b567b3558632b221e
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-11 18:59:24 +0200

    fix(seo):Fixed Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,15 @@ const Home: NextPage = () => {
     <div className='h-screen w-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 max-h-screen overflow-scroll'>
       <Head>
         <title>LS Time Planner / Home</title>
+        <meta property='og:title' content='Loadshedding Time Calculator / Home' />
+        <meta property='og:type' content='website' />
+        <meta
+          property='og:description'
+          content='Struggling to schedule game time with the boys due to loadshedding ? Look no Further, simply make an account, add your corresponding loadshedding area and add your friends ! Then sit back and let the LS Calculator due the stressful work for you !'
+        />
         <meta
           name='description'
-          content='Struggling To Schedule Gaming Sessions With your Friends ? Sign Up and Add your loadshedding Schedules and sit back and enjoy your stress free gaming planning.'
+          content='Struggling to schedule game time with the boys due to loadshedding ? Look no Further, simply make an account, add your corresponding loadshedding area and add your friends ! Then sit back and let the LS Calculator due the stressful work for you !'
         />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <meta property='og:image' content='/Light-bulb.png' />

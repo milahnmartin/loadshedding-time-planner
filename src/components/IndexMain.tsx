50e48a860f4f18a4a9a0acc8ce841be796813c8d
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-12 17:27:40 +0200

    feat(UI): Added Prev Plans BTN

--- src/components/IndexMain.tsx
+++ src/components/IndexMain.tsx
@@ -11,11 +11,18 @@ function IndexMain() {
       <h1 className='font-extrabold text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-primary to-amber-300 py-4 md:text-8xl'>
         LOADSHEDDING ?
       </h1>
-      <Link href='/game/create'>
-        <button className='py-4 px-10 font-bold text-white bg-gradient-to-r from-primary to-secondary rounded-lg hover:from-secondary hover:to-primary'>
-          START PLANNING
-        </button>
-      </Link>
+      <div className='flex w-full h-auto items-center justify-center space-y-5 flex-col md:flex-row md:space-x-5 md:space-y-0'>
+        <Link href='/game/create'>
+          <button className='py-4 px-10 w-[15rem] h-[4rem] font-bold text-white bg-primary rounded-lg transition-all duration-500 ease-in-out hover:bg-sky-700'>
+            NEW PLAN
+          </button>
+        </Link>
+        <Link href='/plans'>
+          <button className='py-4 px-10 w-[15rem] h-[4rem] font-bold border-2 border-primary text-white rounded-lg transition-all duration-300 ease-in-out hover:bg-sky-600 hover:border-white'>
+            SAVED PLANS
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }

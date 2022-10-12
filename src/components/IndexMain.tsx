90d43f9c04ad018725bffcf43f4a46d3717e4ca4
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-12 18:18:55 +0200

    feat(ui): Changed Index UI

--- src/components/IndexMain.tsx
+++ src/components/IndexMain.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 function IndexMain() {
   return (
-    <div className='px-5 border-solid border-red-900 h-full flex justify-start items-center flex-col space-y-9 mt-[8rem]'>
+    <div className='px-5 border-solid border-red-900 h-full flex justify-start items-center flex-col space-y-9 mt-[9rem]'>
       <h1 className='text-white font-bold text-4xl text-center md:text-7xl'>
         STRUGGLING TO PLAN YOUR GAMING
       </h1>
@@ -18,7 +18,7 @@ function IndexMain() {
           </button>
         </Link>
         <Link href='/plans'>
-          <button className='py-4 px-10 w-[15rem] h-[3.5rem] font-bold border-2 border-primary text-white rounded-lg transition-all duration-700 ease-in-out hover:bg-sky-600 hover:border-white'>
+          <button className='py-4 px-10 w-[15rem] h-[3.5rem] font-bold border-2 border-primary text-white rounded-lg transition-all duration-700 ease-in-out hover:bg-sky-700 hover:border-white'>
             SAVED PLANS
           </button>
         </Link>

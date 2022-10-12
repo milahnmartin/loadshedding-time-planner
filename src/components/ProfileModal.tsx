1e7916684bdf8f461734d28c7814ad53574a8b90
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-12 18:30:16 +0200

    feat(MODAL): Changed Modal Text

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -16,15 +16,15 @@ function ProfileModal() {
       id='profile-modal'
       className='h-[8rem] w-[8rem] text-black text-roboto absolute bg-white -left-[5rem] top-12 rounded-md flex items-center justify-start flex-col text-center'
     >
-      <Link href='#'>
+      <Link href='/profile'>
         <div className='h-[50%] w-full hover:bg-gray-300 cursor-pointer py-2 px-1 rounded-t-md outline-none border-none flex items-center justify-center'>
           <h1 className='w-full font-roboto tracking-wide'>PROFILE</h1>
         </div>
       </Link>
 
-      <Link href='#'>
+      <Link href='/plans'>
         <div className='h-[50%] w-full hover:bg-gray-300 cursor-pointer py-2 px-1 rounded-b-md outline-none border-none flex items-center justify-center text-center'>
-          <h1 className='w-full font-roboto tracking-wide'>MATCHES</h1>
+          <h1 className='w-full font-roboto tracking-wide'>PLANS</h1>
         </div>
       </Link>
       <div

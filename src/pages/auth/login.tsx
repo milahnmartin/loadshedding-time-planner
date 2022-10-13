988c3b45f5224298bc4aa8306e0766e5ee4644a5
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-13 17:25:17 +0200

    temp(AUTH): Added BG

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -44,19 +44,21 @@ function login() {
         <h1 className='font-extrabold text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-primary to-amber-300 py-4 md:text-8xl'>
           Let's Get Started !
         </h1>
-        <div className='h-auto w-full flex items-center justify-center space-x-5'>
-          <button
-            onClick={handleGoogleLogin}
-            className='bg-white px-3 py-1 text-black font-bold flex items-center justify-center w-32 rounded-md outline-none border-none hover:bg-gray-300 hover:text-black'
-          >
-            <FcGoogle className='mr-2 h-5 w-5' /> GOOGLE
-          </button>
-          <button
-            onClick={handleTwitterLogin}
-            className='bg-white px-3 py-1 text-black font-bold flex items-center justify-center w-32  rounded-md outline-none border-none hover:bg-gray-300 hover:text-black'
-          >
-            <SiTwitter className='mr-2 h-5 w-5' /> Twitter
-          </button>
+        <div className='shadow-2xl shadow-purple-800 bg-gradient-to-bl from-primary via-purple-500 to-pink-500 h-[25%] w-[15%] p-4 rounded-lg flex items-center'>
+          <div className='h-auto w-full flex flex-col items-center justify-center space-y-8'>
+            <button
+              onClick={handleGoogleLogin}
+              className='bg-white px-3 py-1 text-black font-bold flex items-center justify-center w-32 rounded-md outline-none border-none hover:bg-gray-300 hover:text-black'
+            >
+              <FcGoogle className='mr-2 h-5 w-5' /> GOOGLE
+            </button>
+            <button
+              onClick={handleTwitterLogin}
+              className='bg-white px-3 py-1 text-black font-bold flex items-center justify-center w-32  rounded-md outline-none border-none hover:bg-gray-300 hover:text-black'
+            >
+              <SiTwitter className='mr-2 h-5 w-5' /> Twitter
+            </button>
+          </div>
         </div>
       </div>
       <Footer />

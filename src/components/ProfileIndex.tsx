1bd38686b5339f17bf4a68502bdcd33e53051505
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-13 14:16:50 +0200

    feat(profile): Added Profile

--- src/components/ProfileIndex.tsx
+++ src/components/ProfileIndex.tsx
@@ -1,18 +1,34 @@
 import Image from "next/image";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Logo from "../pages/assets/Light-bulb.png";
 import { auth } from "../utils/firebase-config";
 const ProfileIndex = () => {
   const [user, loading] = useAuthState(auth);
-
   return (
-    <div>
-      <h1>{user?.email}</h1>
-      <Image
-        className='rounded-full border-2 border-solid border-sky-600'
-        src={user?.photoURL as string}
-        width={200}
-        height={200}
-      />
+    <div className='flex items-center flex-col border-2 borde-solid border-white w-full h-full'>
+      <div className='flex h-1/3 w-full flex-col'>
+        <div className='pt-5 flex items-start justify-center w-full'>
+          <Image height={90} width={90} src={Logo} alt='Image of Lightbulb' />
+        </div>
+        <div className='flex items-center justify-center w-full h-full border-2 border-red-900 space-x-5'>
+          {user && (
+            <Image
+              className='rounded-full'
+              height={90}
+              width={90}
+              src={user?.photoURL as string}
+              alt='Image of Lightbulb'
+            />
+          )}
+          <div className='p-2 text-white font-arial tracking-wide'>
+            <h1>{user?.displayName}</h1>
+            <h1>{user?.email}</h1>
+          </div>
+        </div>
+      </div>
+      <div className='flex h-2/3'>
+        <div className='m'></div>
+      </div>
     </div>
   );
 };

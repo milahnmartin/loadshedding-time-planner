de5e481bca50d83f9fc607dc8f83df34a69a9edf
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-14 21:41:46 +0200

    check

--- src/pages/game/[id].tsx
+++ src/pages/game/[id].tsx
@@ -2,7 +2,6 @@ import { NextPage } from "next";
 import Head from "next/head";
 import { createContext } from "react";
 import Footer from "../../components/Footer";
-import GameSpecific from "../../components/GameSpecific";
 import Navbar from "../../components/Navbar";
 export const getServerSideProps = async (context: any) => {
   const { id } = context.query;
@@ -18,7 +17,12 @@ const IdPage: NextPage = ({ id }: any) => {
       </Head>
       <Navbar />
       <GameidContext.Provider value={id}>
-        <GameSpecific />
+        <div className='w-full h-full flex items-center justify-start flex-col pt-10'>
+          <h1 className='font-extrabold text-center pb-5 text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-primary to-amber-300 md:text-7xl'>
+            Start Adding New Players !
+          </h1>
+          {/* <DataControllers /> */}
+        </div>
       </GameidContext.Provider>
       <Footer />
     </div>

341d504bbed4758f20017b4a305f077e3e0a4d90
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-18 18:39:15 +0200

    env variables

--- src/utils/firebase-config.ts
+++ src/utils/firebase-config.ts
@@ -11,22 +11,19 @@ import { getDatabase } from "firebase/database";
 //   measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 // };
 const firebaseConfig = {
-  apiKey: "AIzaSyCza7Oe_qxUqsi8PcmOdrhiV8vmkeby3bM",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
 
-  authDomain: "loadheddint-time-calc.firebaseapp.com",
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
 
-  databaseURL:
-    "https://loadheddint-time-calc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
 
-  projectId: "loadheddint-time-calc",
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
 
-  storageBucket: "loadheddint-time-calc.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
 
-  messagingSenderId: "932828162476",
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 
-  appId: "1:932828162476:web:aa25fefb524f4e7132f209",
-
-  measurementId: "G-QBJCDY4QZB",
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);

f89097a7d4e5915ac939dd8db692b07d2cb62795
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-19 11:14:44 +0200

    fix(profile): Labels Font

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -30,27 +30,27 @@ const plans: NextPage = () => {
       </Head>
       <Navbar />
       <div className='w-full h-full flex'>
-        <div className='w-1/3 h-full flex flex-col items-center justify-start space-y-8 p-5'>
+        <div className='w-1/3 h-full flex flex-col items-center justify-center space-y-8 p-5'>
           <div
             id='profile'
             onClick={() => setCurrentPage("profile")}
-            className='active px-2 py-1 font-roboto font-bold text-white cursor-pointer transition-all duration-500 hover:bg-gray-600 rounded-2xl'
+            className='px-2 py-1 text-white cursor-pointer transition-all duration-500 hover:bg-sky-700 rounded-2xl'
           >
             <h1 className='tracking-wide'>Profile Details</h1>
           </div>
           <div
             id='loadshedding'
             onClick={() => setCurrentPage("loadshedding")}
-            className='font-roboto text-white font-bold cursor-pointer transition-all duration-500 hover:bg-gray-600 rounded-2xl'
+            className='text-white cursor-pointer transition-all duration-500 hover:bg-sky-700 rounded-2xl'
           >
             <h1 className='px-2 py-1 tracking-wide'>Loadshedding Details</h1>
           </div>
           <div
             id='team'
             onClick={() => setCurrentPage("team")}
-            className='px-2 py-1 font-roboto text-white font-bold cursor-pointer transition-all duration-500 hover:bg-gray-600 rounded-2xl'
+            className='px-2 py-1 text-white cursor-pointer transition-all duration-500 hover:bg-sky-700 rounded-2xl'
           >
-            <h1 className='tracking-wide font-main'>Team Settings</h1>
+            <h1 className='tracking-wide'>Team Settings</h1>
           </div>
         </div>
         <div className='w-2/3 h-full flex'>{handlePageRender(currentPage)}</div>

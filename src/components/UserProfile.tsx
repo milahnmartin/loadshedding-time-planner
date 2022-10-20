e4e9f429e5fe0caca4dff59e171f71df053a7c30
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-20 15:28:30 +0200

    feat(modal): Hover on Img Scale Down

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -15,7 +15,7 @@ function UserProfile(props: any) {
         src={props.src}
         width={50}
         height={50}
-        className='rounded-full cursor-pointer hover:scale-110 transition transform duration-200 ease-out'
+        className='rounded-full cursor-pointer hover:scale-95 transition transform duration-200 ease-out'
       />
       {showmodal && <ProfileModal />}
     </div>

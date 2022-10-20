1c264e48d636a5a6df6fce831622d978555ebc0a
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-20 17:10:51 +0200

    feat(navbar):Added Sticky Positioning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,37 +13,37 @@ function Navbar() {
     if (!user && !loading) return setLoginState("LOGIN");
   }, [user, loading]);
   return (
-    <>
-      <div className="h-[5rem] flex place-items-center">
-        <div className="h-auto w-[50%] flex items-center justify-start pl-5">
-          <Image height={50} width={50} src={Logo} alt="Image of Logo" />
-          <h1 className="hidden font-bold text-white text-3xl tracking-wide ml-2 md:inline">
+    <div className='sticky top-0 bg-black z-10'>
+      <div className='h-[5rem] flex place-items-center'>
+        <div className='h-auto w-[50%] flex items-center justify-start pl-5'>
+          <Image height={50} width={50} src={Logo} alt='Image of Logo' />
+          <h1 className='hidden font-bold text-white text-3xl tracking-wide ml-2 md:inline'>
             LS GAME PLANNER
           </h1>
         </div>
-        <div className="h-fit w-[50%] flex items-center navbarfont justify-end pr-5">
-          <h1 className="font-bold text-white tracking-wide ml-5">
-            <Link href="/">HOME</Link>
+        <div className='h-fit w-[50%] flex items-center navbarfont justify-end pr-5'>
+          <h1 className='font-bold text-white tracking-wide ml-5'>
+            <Link href='/'>HOME</Link>
           </h1>
-          <h1 className="font-bold text-white tracking-wide ml-5">
-            <Link href="/docs">DOCS</Link>
+          <h1 className='font-bold text-white tracking-wide ml-5'>
+            <Link href='/docs'>DOCS</Link>
           </h1>
-          <h1 className="font-bold text-white tracking-wide ml-5">
-            <Link href="/instructions">INSTRUCTION</Link>
+          <h1 className='font-bold text-white tracking-wide ml-5'>
+            <Link href='/instructions'>INSTRUCTION</Link>
           </h1>
           {user && !loading ? (
             <UserProfile src={user.photoURL} />
           ) : (
-            <Link href="/auth/login">
-              <button className="px-4 py-1 bg-primary text-white font-bold rounded-lg text-center ml-5">
+            <Link href='/auth/login'>
+              <button className='px-4 py-1 bg-primary text-white font-bold rounded-lg text-center ml-5'>
                 {loginState}
               </button>
             </Link>
           )}
         </div>
       </div>
-      <hr className="my-0 mx-auto w-[100%] h-[0.2rem] bg-cblue rounded border-0 md:my-2" />
-    </>
+      <hr className='my-0 mx-auto w-[100%] h-[0.2rem] bg-cblue rounded border-0 md:my-2' />
+    </div>
   );
 }
 

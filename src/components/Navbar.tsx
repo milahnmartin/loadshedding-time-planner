395ecb531b063dd3c721760ec454d74b13452fb8
MrT1TAN <henri.louw.za@icloud.com>
2022-10-20 22:40:56 +0200

    feat(UI Changes) : Dropdown icons &  Logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,33 +13,35 @@ function Navbar() {
     if (!user && !loading) return setLoginState("LOGIN");
   }, [user, loading]);
   return (
-    <div className='sticky top-0 bg-black z-10'>
-      <div className='h-[5rem] flex place-items-center'>
-        <div className='h-full w-[20%] flex items-center justify-start pl-5 md:w-[50%]'>
-          <Image height={50} width={50} src={Logo} alt='Image of Logo' />
-          <h1 className='hidden font-bold text-white text-3xl tracking-wide ml-2 md:inline'>
+    <div className="sticky top-0 bg-black z-10">
+      <div className="h-[5rem] flex place-items-center">
+        <div className="h-full w-[20%] flex items-center justify-start pl-5 md:w-[50%]">
+          <Link href="/">
+            <Image height={50} width={50} src={Logo} alt="Image of Logo" />
+          </Link>
+          <h1 className="hidden font-bold text-white text-3xl tracking-wide ml-2 md:inline pl-2">
             LS GAME PLANNER
           </h1>
         </div>
-        <div className='h-full w-[80%] space-x-8 flex items-center navbarfont justify-end md:w-[50%] mr-6'>
-          <h1 className='font-bold font-Inter text-white tracking-wide transition-all duration-150 hover:text-cblue'>
-            <Link href='/'>Home</Link>
+        <div className="h-full w-[80%] space-x-8 flex items-center navbarfont justify-end md:w-[50%] mr-6">
+          <h1 className="font-bold font-Inter text-white tracking-wide transition-all duration-150 hover:text-cblue">
+            <Link href="/">Home</Link>
           </h1>
-          <h1 className='font-bold font-Inter text-white tracking-wide transition-all duration-150 hover:text-cblue'>
-            <Link href='/docs'>Docs</Link>
+          <h1 className="font-bold font-Inter text-white tracking-wide transition-all duration-150 hover:text-cblue">
+            <Link href="/docs">Docs</Link>
           </h1>
           {user && !loading ? (
             <UserProfile src={user?.photoURL} />
           ) : (
-            <Link href='/auth/login'>
-              <button className='px-4 py-1 bg-primary text-white font-black tracking-wide font-Inter rounded-lg text-center'>
+            <Link href="/auth/login">
+              <button className="px-4 py-1 bg-primary text-white font-black tracking-wide font-Inter rounded-lg text-center">
                 {loginState}
               </button>
             </Link>
           )}
         </div>
       </div>
-      <hr className='my-0 mx-auto w-[100%] h-[0.2rem] bg-cblue rounded border-0 md:my-2' />
+      <hr className="my-0 mx-auto w-[100%] h-[0.2rem] bg-cblue rounded border-0 md:my-2" />
     </div>
   );
 }

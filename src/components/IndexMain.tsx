9208a60330a2988cb2048158cb83063dcf42b7ef
MrT1TAN <henri.louw.za@icloud.com>
2022-10-20 16:37:16 +0200

    feat (UI changes) : Colours & Buttons

--- src/components/IndexMain.tsx
+++ src/components/IndexMain.tsx
@@ -2,41 +2,43 @@ import Link from "next/link";
 function IndexMain() {
   const arrowIcon = (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 24 24'
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
       strokeWidth={1.5}
-      stroke='currentcolor'
-      className='w-6 h-6'
+      stroke="currentcolor"
+      className="w-6 h-6"
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3'
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
       />
     </svg>
   );
   return (
-    <div className='px-5 border-solid border-red-900 h-full flex justify-start items-center flex-col space-y-9 mt-[9rem]'>
-      <h1 className='text-white font-bold text-4xl text-center md:text-7xl'>
+    <div className="px-5 border-solid border-red-900 h-full flex justify-start items-center flex-col space-y-9 mt-[9rem]">
+      <h1 className="text-white font-bold text-4xl text-center md:text-7xl">
         STRUGGLING TO PLAN YOUR GAMING
       </h1>
-      <h1 className='text-white font-extralight text-5xl text-center md:text-7xl'>
+      <h1 className="text-white font-extralight text-5xl text-center md:text-7xl">
         SESSIONS AROUND{" "}
       </h1>
-      <h1 className='font-extrabold text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple py-4 md:text-8xl'>
+      <h1 className="font-extrabold text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple py-4 md:text-8xl">
         LOADSHEDDING ?
       </h1>
-      <div className='flex w-full h-auto items-center justify-center pt-10 space-y-5 flex-col md:flex-row md:space-x-5 md:space-y-0'>
-        <Link href='/plan/create'>
-          <button className='flex items-center justify-center py-4 px-10 w-[15rem] h-[3.5rem] font-bold text-white bg-primary rounded-lg transition-all duration-700 ease-in-out hover:bg-sky-700'>
-            <div className='mr-5'>NEW PLAN</div>
-            <div className='relative top-[1px]'>{arrowIcon}</div>
+      <div className="flex w-full h-auto items-center justify-center pt-10 flex-col md:flex-row ">
+        <Link href="/plan/create">
+          <button className="relative inline-flex items-center justify-center  p-0.5 mb-2 mr-2 w-[10rem] h-[3rem] overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-cpurple to-caqua  hover:text-white dark:text-white ">
+            NEW PLAN
+            <div className="relative left-[4px]">{arrowIcon}</div>
           </button>
         </Link>
-        <Link href='/plans'>
-          <button className='py-4 px-10 w-[15rem] h-[3.5rem] font-bold border-2 border-primary text-white rounded-lg transition-all duration-700 ease-in-out hover:bg-sky-700 hover:border-white'>
-            SAVED PLANS
+        <Link href="/plans">
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 w-[10rem] h-[3rem] overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white ">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-200 w-[9.5rem] h-[2.5rem] bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0">
+              SAVED PLANS
+            </span>
           </button>
         </Link>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,51 @@ module.exports = {
     "./src/hooks/**/*.tsx",
   ],
   theme: {
-
     extend: {
       colors: {
+        blue: {
+          100: "#d8e4fc",
+          200: "#b1c9f9",
+          300: "#8aaff6",
+          400: "#6394f3",
+          500: "#3c79f0",
+          600: "#3061c0",
+          700: "#244990",
+          800: "#183060",
+          900: "#0c1830",
+        },
+        aqua: {
+          100: "#e1f4f0",
+          200: "#c3e8e1",
+          300: "#a6ddd2",
+          400: "#88d1c3",
+          500: "#6ac6b4",
+          600: "#559e90",
+          700: "#40776c",
+          800: "#2a4f48",
+          900: "#152824",
+        },
+        purple: {
+          100: "#d9d2eb",
+          200: "#b4a5d7",
+          300: "#8e78c2",
+          400: "#694bae",
+          500: "#431e9a",
+          600: "#36187b",
+          700: "#28125c",
+          800: "#1b0c3e",
+          900: "#0d061f",
+        },
         primary: "#002B5B",
         secondary: "#2B4865",
         tersier: "#256D85",
         quaternary: "#8FE3CF",
         caqua: "#6ac6b4",
         cblue: "#3c79f0",
-        cpurple: "#431e9a"
+        cpurple: "#431e9a",
       },
     },
-  
+
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
     },

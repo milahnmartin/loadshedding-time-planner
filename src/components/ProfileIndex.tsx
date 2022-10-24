1d81c73aa8cc2dc22c5290c19bfbbc4b022ffa99
MrT1TAN <henri.louw.za@icloud.com>
2022-10-24 17:56:07 +0200

    Update (Profile Page) : UI Changes

--- src/components/ProfileIndex.tsx
+++ src/components/ProfileIndex.tsx
@@ -1,42 +1,42 @@
 import Image from "next/image";
 import { useAuthState } from "react-firebase-hooks/auth";
-import Logo from "../pages/assets/Light-bulb.png";
+// import Logo from "../pages/assets/Light-bulb.png";
 import { auth } from "../utils/firebase-config";
 const ProfileIndex = () => {
   const [user, loading] = useAuthState(auth);
   return (
-    <div className='p-5 flex items-center flex-col w-full h-full relative'>
-      <Image
-        className='opacity-5 absolute'
-        layout='fill'
+    <div className="p-5 flex items-center flex-col w-full h-full relative">
+      {/* <Image
+        className="opacity-5 absolute"
+        layout="fill"
         src={Logo}
-        alt='Image of Lightbulb'
-      />
-      <div className='flex h-1/3 w-full flex-col'>
-        <div className='pt-5 flex items-start justify-center w-full h-full'>
-          <h1 className='font-extrabold mb-5 text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-primary to-amber-300 py-4 md:text-8xl'>
+        alt="Image of Lightbulb"
+      /> */}
+      <div className="flex h-1/3 w-full flex-col">
+        <div className="pt-5 flex items-start justify-center w-full h-full">
+          <h1 className="font-extrabold mb-5 text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple py-4 md:text-8xl">
             Profile Information
           </h1>
         </div>
-        <div className='p-5 flex items-center flex-col borde-solid w-full h-full'>
+        <div className="p-5 flex items-center flex-col borde-solid w-full h-full">
           {/* Testing profile Card */}
-          <div className='flex h-screen w-full items-center justify-center'>
-            <div className='max-w-md rounded-xl shadow-lg backdrop-blur-2xl bg-white/10'>
-              <div className='flex items-center justify-center w-full h-full space-x-5 pt-8 pb-0 -mb-4'>
+          <div className="flex h-screen w-full items-center justify-center">
+            <div className="max-w-md rounded-xl shadow-lg backdrop-blur-2xl bg-white/10">
+              <div className="flex items-center justify-center w-full h-full space-x-5 pt-8 pb-0 -mb-4">
                 {user && (
                   <Image
-                    className='rounded-full'
+                    className="rounded-full"
                     height={90}
                     width={90}
                     src={user?.photoURL as string}
-                    alt='Image of Lightbulb'
+                    alt="Image of Lightbulb"
                   />
                 )}
               </div>
 
-              <div className='flex flex-row items-center gap-6 p-8'>
-                <h3 className='text-2xl font-bold text-slate-800'>Username:</h3>
-                <p className='text-2xl font-bold text-slate-600'>
+              <div className="flex flex-row items-center gap-6 p-8">
+                <h3 className="text-2xl font-bold text-blue-600">Username:</h3>
+                <p className="text-2xl font-bold text-slate-300">
                   {user?.displayName}
                 </p>
               </div>

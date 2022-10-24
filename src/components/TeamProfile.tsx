1eab3769a2c27c7eab20e66eb5b3b8ef7b7b6067
MrT1TAN <henri.louw.za@icloud.com>
2022-10-24 21:52:03 +0200

    Update (Team Settings UI ) : Color and Layout

--- src/components/TeamProfile.tsx
+++ src/components/TeamProfile.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
-import Logo from "../pages/assets/Light-bulb.png";
+// import Image from "next/image";
+// import Logo from "../pages/assets/Light-bulb.png";
 const TeamProfile = () => {
   return (
-    <div className='p-5 flex items-center flex-col border-2 borde-solid border-white w-full h-full relative'>
-      <Image
-        className='opacity-5 absolute'
-        layout='fill'
+    <div className="p-5 flex items-center flex-col border-2 borde-solid border-white w-full h-full relative">
+      {/* <Image
+        className="opacity-5 absolute"
+        layout="fill"
         src={Logo}
-        alt='Image of Lightbulb'
-      />
-      <h1 className='text-center font-extrabold mb-5 text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-primary to-amber-300 py-4 md:text-8xl'>
-        Team Information
+        alt="Image of Lightbulb"
+      /> */}
+      <h1 className="text-center font-extrabold mb-5 text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple py-4 md:text-8xl">
+        TEAM INFORMATION
       </h1>
     </div>
   );

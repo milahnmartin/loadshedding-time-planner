d01c23096ed6a9f553bfb181a7a32996ed91800f
milahnmartin <milahnmartin.develop@gmail.com>
2022-10-25 23:10:46 +0200

    feat(next):Fixed Modal

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -21,23 +21,27 @@ function ProfileModal() {
       id='profile-modal'
       className='h-[10rem] w-[9rem]  text-black text-Inter absolute bg-white -left-[5rem] top-14 rounded-md flex items-center justify-start flex-col text-center z-50'
     >
-      <Link href='/profile'>
-        <div className='h-[50%] group w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-t-md outline-none border-none flex items-center justify-center'>
-          {<CgProfile className='pl-3 h-9 w-9' />}
-          <h1 className='modal-data w-full font-bold tracking-wide font-Inter'>
-            PROFILE
-          </h1>
-        </div>
-      </Link>
+      <>
+        <Link href='/profile' legacyBehavior>
+          <div className='h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-t-md outline-none border-none flex items-center justify-center'>
+            {<CgProfile className='pl-3 h-9 w-9' />}
+            <h1 className='modal-data w-full font-bold tracking-wide font-Inter'>
+              PROFILE
+            </h1>
+          </div>
+        </Link>
+      </>
+      <>
+        <Link href='/plans' legacyBehavior>
+          <div className='h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-b-md outline-none border-none flex items-center justify-center text-center'>
+            {<BiBookAlt className='pl-3 h-9 w-9' />}
+            <h1 className='modal-data w-full font-bold tracking-wide font-Inter'>
+              PLANS
+            </h1>
+          </div>
+        </Link>
+      </>
 
-      <Link href='/plans'>
-        <div className='h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 outline-none border-none flex items-center justify-center text-center'>
-          {<BiBookAlt className='pl-3 h-9 w-9' />}
-          <h1 className='modal-data w-full font-bold tracking-wide font-Inter'>
-            PLANS
-          </h1>
-        </div>
-      </Link>
       <div
         onClick={handleSignOut}
         className='h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-b-md outline-none border-none flex items-center justify-center text-center'

e3981dad80a28ef0fbda804abc675e8ac9f25fc6
MrT1TAN <henri.louw.za@icloud.com>
2022-10-25 18:29:37 +0200

    Update (Auth UI)

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,7 @@
 import {
   GoogleAuthProvider,
   signInWithPopup,
-  TwitterAuthProvider
+  TwitterAuthProvider,
 } from "firebase/auth";
 import Head from "next/head";
 import Image from "next/image";
@@ -34,53 +34,59 @@ function login() {
     }
   };
   return (
-    <div className='h-screen w-screen overflow-scroll bg-black'>
+    <div className="h-screen w-screen overflow-scroll bg-black">
       <Head>
         <title>LS Time Planner | Login</title>
-        <meta name='description' content='Login to LS Game Planner' />
+        <meta name="description" content="Login to LS Game Planner" />
       </Head>
       <Navbar />
 
-      <div className='h-[80%] flex mt-[3.5rem]'>
-        <div className='w-1/4 justify-end px-5 hidden md:flex'>
-          <div className='h-full items-center justify-center '>
-            <div className='h-[45%] w-full flex justify-center'>
-              <span className='h-full w-[3px] bg-gradient-to-t from-caqua via-cblue to-cpurple'></span>
+      <div className="h-[80%] flex mt-[3.5rem]">
+        <div className="w-1/4 justify-end px-5 hidden md:flex">
+          <div className="h-full items-center justify-center ">
+            <div className="h-[45%] w-full flex justify-center">
+              <span className="h-full w-[3px] bg-gradient-to-t from-caqua via-cblue to-cpurple"></span>
             </div>
-            <div className='h-[10%] w-full flex items-center justify-center'>
+            <div className="h-[10%] w-full flex items-center justify-center">
               <Image
-                className='w-fit'
+                className="w-fit"
                 width={60}
                 height={60}
                 src={Logo}
-                alt='Image of Logo'
+                alt="Image of Logo"
               />
             </div>
-            <div className='h-[45%] w-full flex justify-center'>
-              <span className='h-full w-[3px] bg-gradient-to-b from-caqua via-cblue to-cpurple'></span>
+            <div className="h-[45%] w-full flex justify-center">
+              <span className="h-full w-[3px] bg-gradient-to-b from-caqua via-cblue to-cpurple"></span>
             </div>
           </div>
         </div>
-        <div className='w-full flex items-center justify-center flex-col border-2 md:w-3/4'>
-          <div className=' h-1/4 p-2 flex items-center justify-center text-center'>
-            <h1 className=' text-center font-extrabold text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple py-4 md:text-6xl'>
+        <div className="w-full flex items-center justify-center flex-col md:3/4">
+          <div className=" h-1/4 p-2 flex items-center justify-center text-center">
+            <h1 className=" text-center font-extrabold text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple py-4 md:text-6xl">
               TIME TO LOG IN!
             </h1>
           </div>
-          <div className='h-2/4 flex items-center justify-center'>
-            <div className='w-fit h-fit p-24 rounded-xl shadow-lg backdrop-blur-2xl bg-white/10'>
-              <div className='h-auto w-full flex flex-col items-center justify-center space-y-8'>
+          <div className="h-2/4 flex items-center justify-center">
+            <div className="w-fit h-fit p-16 pt-5 rounded-xl bg-white/10">
+              <h1 className=" font-bold text-white text-center text-[1.3rem] pb-3">
+                Login with:{" "}
+              </h1>
+              <div className="h-auto w-full flex flex-col items-center justify-center space-y-8">
                 <button
                   onClick={handleGoogleLogin}
-                  className='bg-white px-3 py-1 text-black font-bold flex items-center justify-center w-32 rounded-md outline-none border-none transition-all duration-200 hover:bg-white/25'
+                  className="bg-white px-3 py-1 text-black font-bold flex items-center justify-center w-32 rounded-md outline-none border-none transition-all duration-200 hover:bg-white/25"
                 >
-                  <FcGoogle className='h-8 w-8' /> GOOGLE
+                  <FcGoogle className="h-8 w-8" /> GOOGLE
                 </button>
+                <div className="inline-flex justify-center items-center w-full">
+                  <hr className="my-8 w-64 h-px bg-gray-400 border-1 " />
+                </div>
                 <button
                   onClick={handleTwitterLogin}
-                  className='bg-white px-3 py-1 text-black font-bold flex items-center justify-center w-32 rounded-md outline-none border-none transition-all duration-200 hover:bg-white/25'
+                  className="bg-white px-3 py-1 text-black font-bold flex items-center justify-center w-32 rounded-md outline-none border-none transition-all duration-200 hover:bg-white/25"
                 >
-                  <SiTwitter className='h-8 w-8' /> Twitter
+                  <SiTwitter className="h-8 w-8" /> Twitter
                 </button>
               </div>
             </div>

f5d4bebae3eeaa6ff631101a2368e029886125d8
milahnmartin <milahnmartin.develop@gmail.com>
2022-11-01 17:41:09 +0200

    feat(navbar):Logo Size & Profile Pic

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -23,13 +23,13 @@ function UserProfile(props: any) {
   }, []);
 
   return (
-    <div className='rounded-full h-fit flex items-center justify-center relative border-2 border-cblue'>
+    <div className='rounded-full h-fit flex items-center justify-center relative p-[2px] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
       <Image
         id='profile-img'
         onClick={handleShowModal}
         src={props.src}
-        width={50}
-        height={50}
+        width={55}
+        height={55}
         alt='profile img'
         className='rounded-full cursor-pointer hover:scale-95 transition-all duration-200'
       />

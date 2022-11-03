28c693de764cd9894d7b02e4b6f1515f83437663
milahnmartin <milahnmartin.develop@gmail.com>
2022-11-03 12:49:57 +0200

    feat(profileModal):Text White on Hover

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -19,13 +19,13 @@ function ProfileModal() {
   return (
     <div
       id='profile-modal'
-      className='h-[10rem] w-[9rem]  text-black text-Inter absolute bg-white -left-[5rem] top-14 rounded-md flex items-center justify-start flex-col text-center z-50'
+      className='h-[10rem] w-[9rem]  text-black text-Inter absolute bg-white -left-[5rem] top-16 rounded-md flex items-center justify-start flex-col text-center z-50'
     >
       <>
         <Link href='/profile' legacyBehavior>
-          <div className='h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-t-md outline-none border-none flex items-center justify-center'>
-            {<CgProfile className='pl-3 h-9 w-9' />}
-            <h1 className='modal-data w-full font-bold tracking-wide font-Inter'>
+          <div className='group h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-t-md outline-none border-none flex items-center justify-center'>
+            <CgProfile className='pl-3 h-9 w-9 group-hover:text-white' />
+            <h1 className='modal-data w-full font-bold tracking-wide font-Inter group-hover:text-white'>
               PROFILE
             </h1>
           </div>
@@ -33,9 +33,9 @@ function ProfileModal() {
       </>
       <>
         <Link href='/plans' legacyBehavior>
-          <div className='h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-b-md outline-none border-none flex items-center justify-center text-center'>
-            {<BiBookAlt className='pl-3 h-9 w-9' />}
-            <h1 className='modal-data w-full font-bold tracking-wide font-Inter'>
+          <div className='group h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-b-md outline-none border-none flex items-center justify-center text-center'>
+            <BiBookAlt className='pl-3 h-9 w-9 group-hover:text-white' />
+            <h1 className='modal-data w-full font-bold tracking-wide font-Inter group-hover:text-white'>
               PLANS
             </h1>
           </div>
@@ -44,10 +44,10 @@ function ProfileModal() {
 
       <div
         onClick={handleSignOut}
-        className='h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-b-md outline-none border-none flex items-center justify-center text-center'
+        className='group h-[50%] w-full transition-all duration-300 hover:bg-cblue cursor-pointer py-2 px-1 rounded-b-md outline-none border-none flex items-center justify-center text-center'
       >
-        {<CgLogOut className='pl-3 h-8 w-9' />}
-        <h1 className='modal-data w-full font-bold tracking-wide font-Inter'>
+        <CgLogOut className='pl-3 h-8 w-9 group-hover:text-white' />
+        <h1 className='modal-data w-full font-bold tracking-wide font-Inter group-hover:text-white'>
           {signOutStatus ? "LOGGING OUT ..." : "LOGOUT"}
         </h1>
       </div>

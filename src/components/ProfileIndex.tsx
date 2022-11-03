69b1be26a87cd3b399deaf87751c62fa18b7bf59
MrT1TAN <henri.louw.za@icloud.com>
2022-11-03 12:35:17 +0200

    Update (Profile UI)

--- src/components/ProfileIndex.tsx
+++ src/components/ProfileIndex.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../utils/firebase-config";
+import { Player } from "@lottiefiles/react-lottie-player";
 const ProfileIndex = () => {
   const [user, loading] = useAuthState(auth);
   return (
@@ -9,8 +10,15 @@ const ProfileIndex = () => {
         <h1 className='font-extrabold mb-5 text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple pt-4 md:text-6xl'>
           PROFILE INFORMATION
         </h1>
+        <Player
+          src='https://assets7.lottiefiles.com/packages/lf20_xyadoh9h.json'
+          className='player w-[300px] h-[300px] '
+          autoplay
+          loop
+          speed={0.5}
+        />
       </div>
-      <div className='flex h-full w-full items-center justify-center'>
+      <div className='flex h-full w-full items-start justify-center pt-7'>
         <div className='rounded-xl w-[30rem] h-auto mx-auto bg-gradient-to-r p-[6px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
           <div className='flex flex-col items-center h-full bg-black text-white rounded-lg p-4 '>
             {user && (

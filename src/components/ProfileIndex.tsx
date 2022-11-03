18944285b9f5d1827bd4cce6fc5f91c056c97f65
milahnmartin <milahnmartin.develop@gmail.com>
2022-11-03 17:38:27 +0200

    feat(profile)Copy UUID, Shows Info

--- src/components/ProfileIndex.tsx
+++ src/components/ProfileIndex.tsx
@@ -1,13 +1,23 @@
-import Image from "next/image";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../utils/firebase-config";
 import { Player } from "@lottiefiles/react-lottie-player";
+import Image from "next/image";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { MdContentCopy } from "react-icons/md";
+import { toast } from "react-toastify";
+import { auth } from "../utils/firebase-config";
 const ProfileIndex = () => {
   const [user, loading] = useAuthState(auth);
+  useEffect(() => {
+    const myBubble = document.getElementById("bubble-test");
+    myBubble?.addEventListener("click", (e) => {
+      console.log(`Target: ${e.target}`);
+      console.log(`Current Target: ${e.currentTarget}`);
+    });
+  }, []);
   return (
-    <div className='p-5 w-full h-full flex items-center flex-col'>
+    <div className='w-full h-full flex items-center flex-row overflow-y-hidden'>
       <div className='w-full flex flex-col items-center justify-start'>
-        <h1 className='font-extrabold mb-5 text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple pt-4 md:text-6xl'>
+        <h1 className='font-extrabold text-4xl animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-caqua via-cblue to-cpurple pt-4 md:text-6xl'>
           PROFILE INFORMATION
         </h1>
         <Player
@@ -18,23 +28,45 @@ const ProfileIndex = () => {
           speed={0.5}
         />
       </div>
-      <div className='flex h-full w-full items-start justify-center pt-7'>
-        <div className='rounded-xl w-[30rem] h-auto mx-auto bg-gradient-to-r p-[6px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
-          <div className='flex flex-col items-center h-full bg-black text-white rounded-lg p-4 '>
-            {user && (
-              <Image
-                className='rounded-full'
-                height={90}
-                width={90}
-                src={user?.photoURL as string}
-                alt='Image of Lightbulb'
-              />
-            )}
-            <div className='flex flex-row items-center gap-4 p-6 pb-1'>
-              <h3 className='text-2xl font-bold text-slate-300'>Username:</h3>
-              <p className='text-2xl font-bold text-slate-300'>
-                {user?.displayName}
-              </p>
+      <div className='h-full w-full'>
+        <div className='rounded-xl w-full h-full mx-auto bg-gradient-to-r p-[6px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
+          <div className='flex flex-col items-center justify-center h-full bg-black text-white rounded-lg p-4'>
+            <div className='flex items-center justify-center h-full w-[50%]'>
+              {user && (
+                <Image
+                  className='rounded-full'
+                  height={120}
+                  width={120}
+                  src={user?.photoURL as string}
+                  alt='Image of Lightbulb'
+                />
+              )}
+            </div>
+
+            <div className='w-full h-full flex flex-col items-center justify-center gap-4 p-6 pb-1 text-center'>
+              <div className='flex space-x-2 items-center justify-start text-center w-full'>
+                <h3 className='text-2xl font-bold text-slate-300'>Username:</h3>
+                <p className='text-2xl font-bold text-cblue'>{user?.displayName}</p>
+              </div>
+              <div className='flex space-x-2 items-center justify-start text-center w-full'>
+                <h3 className='text-2xl font-bold text-slate-300'>UUID:</h3>
+                <p className='text-xl font-bold text-cblue'>{user?.uid}</p>
+                <span>
+                  <MdContentCopy
+                    className='cursor-pointer h-full w-[20px]'
+                    onClick={() => {
+                      navigator.clipboard.writeText(user?.uid as string);
+                      toast.success("UID Copied to Clipboard");
+                    }}
+                  />
+                </span>
+              </div>
+              <div className='flex space-x-2 items-center justify-start text-center w-full'>
+                <h3 className='text-2xl font-bold text-slate-300'>Email:</h3>
+                <p className='text-2xl font-bold text-cblue'>
+                  {user?.email ? user?.email : "No Email Provided"}
+                </p>
+              </div>
             </div>
           </div>
         </div>

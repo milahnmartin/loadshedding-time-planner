3ed9dfa4a7857a958aca9611623532f7e67083e0
milahnmartin <milahnmartin.develop@gmail.com>
2022-11-07 11:49:24 +0200

    feat(api):Added Test Param

--- src/pages/api/sepush/[id]/[date].ts
+++ src/pages/api/sepush/[id]/[date].ts
@@ -11,7 +11,7 @@ export default async function fetchUserLSTimesById(
   const pushID = req.query.id;
   const pushDate = req.query.date;
   const lsData = await fetch(
-    `https://developer.sepush.co.za/business/2.0/area?id=${pushID}`,
+    `https://developer.sepush.co.za/business/2.0/area?id=${pushID}&test=current`,
     {
       method: "GET",
       headers: {

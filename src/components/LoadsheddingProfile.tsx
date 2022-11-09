a03454fda3b43c8fa0721b8b6fb0550dc1248e7d
MrT1TAN <henri.louw.za@icloud.com>
2022-11-09 18:39:01 +0200

    Revert "Merge branch 'development' of https://github.com/milahnmartin/eskom-time-compiler into development"
    This reverts commit 9eba3ad915a8791940c85adafbc747283c34036f, reversing
    changes made to 316fc617519dd37b337797ed527e049dc21677da.

--- src/components/LoadsheddingProfile.tsx
+++ src/components/LoadsheddingProfile.tsx
@@ -4,6 +4,7 @@ import classNames from "classnames";
 import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { AiOutlineSearch } from "react-icons/ai";
+import { ThreeDots } from "react-loading-icons";
 import { toast } from "react-toastify";
 import { v4 as uuidv4 } from "uuid";
 import useFetchArea from "../hooks/useFetchArea";
@@ -154,87 +155,20 @@ const LoadsheddingProfile = () => {
                   speed={0.5}
                 />
               </div>
-            ) : SavedAreaData ? (
-              <div className='w-full h-full flex items-center flex-col space-y-6 justify-center text-center font-Inter tracking-wide'>
-                <div>
-                  <h1 className='text-blue-500 text-xl font-black'>AREA ID:</h1>
-                  <h1 className='text-blue-200 font-bold'>{SavedAreaData.id}</h1>
-                </div>
-                <div>
-                  <h1 className='text-blue-500 text-xl font-black'>AREA REGION:</h1>
-                  <h1 className='text-blue-200 font-bold'>{SavedAreaData.region}</h1>
-                </div>
-                <div>
-                  <h1 className='text-blue-500 text-xl font-black'>AREA NAME:</h1>
-                  <h1 className='text-blue-200 font-bold'>{SavedAreaData.name}</h1>
-                </div>
-              </div>
-            ) : (
-              <div className='w-full h-full flex items-center flex-col space-y-11 justify-center text-center font-Inter font-black tracking-wide'>
-                <h1 className='text-cblue font-Inter font-black text-xl tracking-wide'>
-                  NO SAVED AREA
-                </h1>
-                <button
-                  onClick={() => document.getElementById("areaSearch")?.focus()}
-                  className='relative inline-flex items-center justify-center p-0.5  w-[10rem] h-[3rem] overflow-hidden text-sm font-medium text-gray-900 rounded-[1.15rem] group bg-gradient-to-br from-c2aqua via-c2blue to-c2purple group-hover:from-c2aqua group-hover:via-c2blue group-hover:to-c2purple hover:text-white dark:text-white '
-                >
-                  <span className='relative px-5 py-2.5 transition-all ease-in duration-200  w-[9.5rem] h-[2.5rem] bg-white dark:bg-gray-900 rounded-[1rem] group-hover:bg-opacity-0 font-bold'>
-                    SET AREA
-                  </span>
-                </button>
-              </div>
             )}
-          </div>
-        </div>
-      </div>
-      <div className='w-3/4 h-full flex flex-col items-center justify-center space-y-2 px-4'>
-        {/* <h1 className='text-white font-Inter font-black text-2xl tracking-widest '>
-            Search Your Area:
-          </h1> */}
-        <span className='w-full px-1 py-1 bg-gradient-to-t from-cpurple via-pink-600 to-cblue rounded-md relative'>
-          <span className='absolute flex items-center h-full w-fit px-2 -top-[0px]'>
-            <AiOutlineSearch size={25} fill='white' />
-          </span>
-          <input
-            id='areaSearch'
-            className='bg-black px-4 text-center py-2 w-full text-lg rounded-md outline-none font-black text-white tracking-wide font-inter focus:ring-4 focus:ring-c2aqua placeholder:bg-clip-text placeholder:text-transparent placeholder:bg-gradient-to-r placeholder:from-pink-500 placeholder:via-red-500 placeholder:to-yellow-500 placeholder:text-center placeholder:font-Inter placeholder:font-black'
-            type='text'
-            name='loadshedding-area'
-            placeholder='Search Your Area: Waterkloof, Durbanville etc...'
-            value={areaInput}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setareaInput(e.currentTarget.value)
-            }
-          />
-        </span>
-
-        <div className='overflow-y-scroll w-full h-full  items-center justify-center'>
-          {/* <div className='overflow-y-scroll w-full h-full'> */}
-          {AreaDataLoading && (
-            <div className='w-full h-full flex items-center justify-center'>
-              {/* <ThreeDots fill='#3c79f0' /> */}
-              <Player
-                src='https://assets2.lottiefiles.com/private_files/lf30_3vhjjbex.json'
-                className='player w-[30%] h-[30%] '
-                autoplay
-                loop
-                speed={0.5}
-              />
-            </div>
-          )}
-          {AreaData &&
-            AreaData?.map((area: IAreaData) => {
-              return (
-                <AreaLabels
-                  key={uuidv4()}
-                  id={area.id!}
-                  name={area.name!}
-                  region={area.region!}
-                  cbSetArea={handleSetArea}
-                />
-              );
-            })}
-          {/* <AreaLabels
+            {AreaData &&
+              AreaData?.map((area: IAreaData) => {
+                return (
+                  <AreaLabels
+                    key={uuidv4()}
+                    id={area.id!}
+                    name={area.name!}
+                    region={area.region!}
+                    cbSetArea={handleSetArea}
+                  />
+                );
+              })}
+            {/* <AreaLabels
               key={uuidv4()}
               id={"nelsonmandelabay-15-waterkloofwkkarea30"}
               name={"nelsonmandelabay-15-waterkloofwkkarea30"}
@@ -262,9 +196,10 @@ const LoadsheddingProfile = () => {
               region={"Nelson Mandela Bay Municipality"}
               cbSetArea={() => console.log("YES")}
             /> */}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

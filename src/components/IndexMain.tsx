57f2f6a7ed2011201cf24412403dbc7d27d9143a
milahnmartin <milahnmartin.develop@gmail.com>
2022-11-10 23:30:19 +0200

    feat(index):UI

--- src/components/IndexMain.tsx
+++ src/components/IndexMain.tsx
@@ -84,17 +84,17 @@ const IndexMain = () => {
       <div className='flex w-full h-auto items-center justify-center pt-10 flex-col md:flex-row '>
         <button
           onClick={handleNewPlan}
-          className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 w-[10rem] h-[3rem] overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-cpurple to-caqua  hover:text-white dark:text-white '
+          className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 w-[10rem] h-[3rem] overflow-hidden text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-cpurple to-caqua  hover:text-white dark:text-white '
         >
           NEW PLAN
-          <div className='relative left-[6px] top-[.5px] transition-all duration-500 group-hover:text-orange-500'>
+          <div className='relative left-[6px] top-[.5px] transition-all duration-250 group-hover:text-yellow-500'>
             {arrowIcon}
           </div>
         </button>
 
         <Link href='/plans'>
-          <button className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 w-[10rem] h-[3rem] overflow-hidden text-sm font-medium text-gray-900 rounded-full group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '>
-            <span className='relative px-5 py-2.5 transition-all ease-in duration-200 w-[9.5rem] h-[2.5rem] bg-white dark:bg-gray-900 rounded-full group-hover:bg-opacity-0'>
+          <button className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 w-[10rem] h-[3rem] overflow-hidden text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '>
+            <span className='relative px-5 py-2.5 transition-all ease-in duration-200 w-[9.5rem] h-[2.5rem] bg-white dark:bg-black rounded-full group-hover:bg-opacity-0'>
               SAVED PLANS
             </span>
           </button>

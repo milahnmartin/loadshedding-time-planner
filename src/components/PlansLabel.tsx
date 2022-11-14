fd44407e6a9f97497b581ff7c7c87fdd0f9334a3
MrT1TAN <henri.louw.za@icloud.com>
2022-11-14 17:01:32 +0200

    Update (Plans) : UI

--- src/components/PlansLabel.tsx
+++ src/components/PlansLabel.tsx
@@ -10,38 +10,45 @@ const PlansLabel = ({ plan }: any) => {
     plan_createdAt,
   } = plan;
   return (
-    <div className='overflow-y-scroll flex flex-col bg-gradient-to-r from-cblue via-cpurple to-c2purple p-1 w-[25rem] h-[20rem] rounded-xl'>
-      <span className='flex flex-col w-full h-full rounded-xl'>
-        <div className='w-full h-full flex items-center justify-center flex-col space-y-2 overflow-y-scroll'>
-          <h1>Plan ID</h1>
-          <h1 className='text-white text-center font-Inter font-bold'>{plan_id}</h1>
+    // <div className='overflow-y-scroll flex flex-col bg-gradient-to-r from-cblue via-cpurple to-c2purple p-1 w-[25rem] h-[20rem] rounded-xl border-2'>
+    <div className='rounded-xl w-[25rem] h-[20rem] bg-gradient-to-r p-[5px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
+      <div className='flex flex-col items-center h-full bg-slate-800 text-white rounded-lg p-4'>
+        <span className='flex flex-col w-full h-full rounded-xl'>
+          <div className='w-full h-full flex items-center justify-center flex-col space-y-2 overflow-y-scroll'>
+            <h1>Plan ID</h1>
+            <h1 className='text-white text-center font-Inter font-bold'>
+              {plan_id}
+            </h1>
 
-          <h1>Plan Created On:</h1>
-          <pre className='text-white text-center font-bold'>
-            {new Date(plan_createdAt).toUTCString()}
-          </pre>
-          {plan_lsTimes.map((time: string) => {
-            return (
-              <h1 className='text-white text-center font-bold' key={uuidv4()}>
-                {time}
-              </h1>
-            );
-          })}
-          {plan_authorizedUsers.map((user: string) => {
-            return <pre className='text-white text-center'>{user}</pre>;
-          })}
-          {plan_authorizedTeams.map((user: string) => {
-            return <pre className='text-white text-center'>{user}</pre>;
-          })}
-        </div>
-        <Link href={`/dashboard/${plan_id}`}>
-          <button className='relative w-full inline-flex items-center justify-center px-[2px] py-[2px]  h-[3rem] overflow-hidden text-sm font-medium text-gray-900 rounded-xl group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '>
-            <span className='text-center flex items-center justify-center relative transition-all ease-in duration-200 w-full h-full bg-white dark:bg-gray-900 rounded-xl group-hover:bg-opacity-0'>
-              VISIT
-            </span>
-          </button>
-        </Link>
-      </span>
+            <h1>Plan Created On:</h1>
+            <pre className='text-white text-center font-bold'>
+              {new Date(plan_createdAt).toUTCString()}
+            </pre>
+            {plan_lsTimes.map((time: string) => {
+              return (
+                <h1 className='text-white text-center font-bold' key={uuidv4()}>
+                  {time}
+                </h1>
+              );
+            })}
+            {plan_authorizedUsers.map((user: string) => {
+              return <pre className='text-white text-center'>{user}</pre>;
+            })}
+            {plan_authorizedTeams.map((user: string) => {
+              return <pre className='text-white text-center'>{user}</pre>;
+            })}
+          </div>
+          <div className='flex items-center justify-center'>
+            <Link href={`/dashboard/${plan_id}`}>
+              <button className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 w-[10rem] h-[3rem] overflow-hidden text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '>
+                <span className='relative px-5 py-2.5 transition-all ease-in duration-200 w-[9.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-full group-hover:bg-opacity-0'>
+                  View Plan
+                </span>
+              </button>
+            </Link>
+          </div>
+        </span>
+      </div>
     </div>
   );
 };

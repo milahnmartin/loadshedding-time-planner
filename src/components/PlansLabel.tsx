471042e4bc0e2f2c4f465088f49266183c85e6d4
MrT1TAN <henri.louw.za@icloud.com>
2022-11-22 14:42:34 +0200

    Update (Plans Label): Spacing

--- src/components/PlansLabel.tsx
+++ src/components/PlansLabel.tsx
@@ -4,22 +4,22 @@ import Link from "next/link";
 import { BiRightArrowAlt } from "react-icons/bi";
 import { BsCalendar } from "react-icons/bs";
 import { HiOutlineKey } from "react-icons/hi";
-import {v1 as uuidv1} from 'uuid'
-import {MdDeleteForever} from 'react-icons/md'
+import { v1 as uuidv1 } from "uuid";
+import { MdDeleteForever } from "react-icons/md";
 type planData = {
   plan_id: string;
-    plan_lsTimes:string[];
-    plan_authorizedUsers:string[];
-    plan_authorizedTeams:string[];
-    plan_createdAt:string;
-}
+  plan_lsTimes: string[];
+  plan_authorizedUsers: string[];
+  plan_authorizedTeams: string[];
+  plan_createdAt: string;
+};
 type PlansLabelProps = {
-  plan:planData;
-  deleteCB:(plan_id:string)=>void;
-}
+  plan: planData;
+  deleteCB: (plan_id: string) => void;
+};
 
 import supabase from "../utils/supabase-config";
-const PlansLabel = ({plan,deleteCB}:PlansLabelProps) => {
+const PlansLabel = ({ plan, deleteCB }: PlansLabelProps) => {
   const {
     plan_id,
     plan_lsTimes,
@@ -28,10 +28,9 @@ const PlansLabel = ({plan,deleteCB}:PlansLabelProps) => {
     plan_createdAt,
   } = plan;
 
-
   return (
     // <div className='overflow-y-scroll flex flex-col bg-gradient-to-r from-cblue via-cpurple to-c2purple p-1 w-[25rem] h-[20rem] rounded-xl border-2'>
-    <div className='rounded-xl w-[25rem] h-[30rem] bg-gradient-to-r p-[3px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
+    <div className='rounded-xl w-[25rem] h-[25rem] bg-gradient-to-r p-[3px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
       <div className='flex flex-col h-full w-full bg-slate-800 text-white rounded-lg'>
         <span className='flex flex-col items-center w-full h-fit'>
           <Player
@@ -42,42 +41,53 @@ const PlansLabel = ({plan,deleteCB}:PlansLabelProps) => {
             speed={0.8}
           />
         </span>
-        <div className='w-full h-full flex items-center justify-evenly flex-wrap content-center overflow-y-scroll overflow-x-hidden flex-col'>
-          <span className='flex items-center justify-center flex-col'>
+        <div className='w-full h-full flex items-center justify-center flex-wrap content-center overflow-y-scroll overflow-x-hidden flex-col'>
+          <span className='flex items-center justify-center  flex-col'>
             <HiOutlineKey className='text-[1.15rem] align-center justify-center' />
             <h1 className='text-blue-500 text-center font-Inter font-black text-lg'>
               PLAN ID:
             </h1>
-               <h1 className='text-blue-200 text-center text-lg font-bold '>{plan_id}</h1>
+            <h1 className='text-blue-200 text-center text-lg font-bold pb-5'>
+              {plan_id}
+            </h1>
           </span>
-
-
 
           <span className='flex items-center justify-center flex-col'>
             <BsCalendar className='text-[1.075rem] align-center justify-center' />
             <h1 className='text-blue-500 text-center font-Inter font-black text-lg'>
               PLAN CREATED:
             </h1>
-             <h1 className='text-blue-200 text-center text-lg font-bold '>
-            {new Date(plan_createdAt).toUTCString()}
-          </h1>
+            <h1 className='text-blue-200 text-center text-lg font-bold '>
+              {new Date(plan_createdAt).toUTCString()}
+            </h1>
           </span>
 
           {plan_lsTimes.map((time: string) => {
             return (
-              <h1 className='text-blue-500 text-center font-black text-lg' key={uuidv1()}>
+              <h1
+                className='text-blue-500 text-center font-black text-lg'
+                key={uuidv1()}
+              >
                 {time}
               </h1>
             );
           })}
           {plan_authorizedUsers.map((user: string) => {
-            return <pre key={uuidv1()} className='text-white text-center'>{user}</pre>;
+            return (
+              <pre key={uuidv1()} className='text-white text-center'>
+                {user}
+              </pre>
+            );
           })}
           {plan_authorizedTeams.map((user: string) => {
-            return <pre key={uuidv1()} className='text-white text-center'>{user}</pre>;
+            return (
+              <pre key={uuidv1()} className='text-white text-center'>
+                {user}
+              </pre>
+            );
           })}
         </div>
-        <div className='flex items-center justify-center w-full h-fit py-2 space-x-4'>
+        <div className='flex items-center justify-center w-full h-fit pb-4 space-x-4'>
           <Link href={`/dashboard/${plan_id}`}>
             <button className='relative flex items-center justify-center  w-[10rem] h-[3rem] text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '>
               <span className='relative px-5 py-2.5 group-hover:px-0 transition-all ease-in duration-200 w-[9.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-full group-hover:bg-opacity-0'>
@@ -88,7 +98,11 @@ const PlansLabel = ({plan,deleteCB}:PlansLabelProps) => {
               </span>
             </button>
           </Link>
-          <MdDeleteForever title="Delete Plan" onClick={()=>deleteCB(plan_id)} className='cursor-pointer text-[1.5rem] align-center justify-center hover:text-red-700 transition-all duration-300' />
+          <MdDeleteForever
+            title='Delete Plan'
+            onClick={() => deleteCB(plan_id)}
+            className='cursor-pointer text-[1.5rem] align-center justify-center hover:text-red-700 transition-all duration-300'
+          />
         </div>
       </div>
     </div>
--- src/pages/plans.tsx
+++ src/pages/plans.tsx
@@ -1,60 +1,62 @@
 import Footer from "@comps/Footer";
 import Navbar from "@comps/Navbar";
 import PlansLabel from "@comps/PlansLabel";
-import {v1 as uuidv1} from "uuid";
+import { v1 as uuidv1 } from "uuid";
 import { Player } from "@lottiefiles/react-lottie-player";
 import { NextPage } from "next";
 import Head from "next/head";
 import useFetchSavedPlans from "../hooks/useFetchSavedPlans";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 import supabase from "../utils/supabase-config";
 
 const plans: NextPage = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const {
     data: savedPlans,
     isFetching: savedPlansIsFetching,
-      refetch: refetchSavedPlans,
-      // eslint-disable-next-line react-hooks/rules-of-hooks
+    refetch: refetchSavedPlans,
+    // eslint-disable-next-line react-hooks/rules-of-hooks
   } = useFetchSavedPlans();
-    const handlePlanDelete = async (plan_id:string) => {
-        if(!plan_id){
-            toast.error("No Plan Id Provided");
-            return;
-        }
-        const {error} = await supabase
-            .from("user_plans")
-            .delete()
-            .eq("plan_id",plan_id);
-
-        if(error){
-            toast.error(error.message);
-            return;
-        }
-
-        toast.success(`Plan ${plan_id} Deleted Successfully`);
-        await refetchSavedPlans();
+  const handlePlanDelete = async (plan_id: string) => {
+    if (!plan_id) {
+      toast.error("No Plan Id Provided");
+      return;
     }
+    const { error } = await supabase
+      .from("user_plans")
+      .delete()
+      .eq("plan_id", plan_id);
+
+    if (error) {
+      toast.error(error.message);
+      return;
+    }
+
+    toast.success(`Plan ${plan_id} Deleted Successfully`);
+    await refetchSavedPlans();
+  };
   return (
     <div className='h-screen w-screen overflow-y-scroll'>
       <Head>
         <title>LS Planner / Plans</title>
       </Head>
       <Navbar />
-        <div className='flex min-h-[90vh] max-h-fit w-full flex-wrap content-center items-center justify-center overflow-y-scroll gap-2 py-4'>
-          {savedPlansIsFetching ? (
-            <Player
-              src='https://assets2.lottiefiles.com/private_files/lf30_3vhjjbex.json'
-              className='player w-[30%] h-[30%] '
-              autoplay
-              loop
-              speed={0.5}
-            />
-          ) : (
-            savedPlans?.map((plan: any) => {
-              return <PlansLabel plan={plan} key={uuidv1()} deleteCB={handlePlanDelete} />;
-            })
-          )}
+      <div className='flex min-h-[90vh] max-h-fit w-full flex-wrap content-center items-center justify-center overflow-y-scroll gap-2 py-4'>
+        {savedPlansIsFetching ? (
+          <Player
+            src='https://assets2.lottiefiles.com/private_files/lf30_3vhjjbex.json'
+            className='player w-[30%] h-[30%] '
+            autoplay
+            loop
+            speed={0.5}
+          />
+        ) : (
+          savedPlans?.map((plan: any) => {
+            return (
+              <PlansLabel plan={plan} key={uuidv1()} deleteCB={handlePlanDelete} />
+            );
+          })
+        )}
       </div>
       <Footer />
     </div>

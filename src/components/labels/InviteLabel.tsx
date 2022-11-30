370d36abb3bb6cf3ff2425d2474484171862baf9
MrT1TAN <henri.louw.za@icloud.com>
2022-11-30 13:33:03 +0200

    Update (Invite Label) : Styling

--- src/components/labels/InviteLabel.tsx
+++ src/components/labels/InviteLabel.tsx
@@ -1,67 +1,90 @@
 import { uuidv4 } from "@firebase/util";
 import { Player } from "@lottiefiles/react-lottie-player";
+import { FaCheck } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { BsCalendar } from "react-icons/bs";
+import { HiOutlineKey } from "react-icons/hi";
+import { FaUserCircle } from "react-icons/fa";
 import Link from "next/link";
 
-const InviteLabel = ({ plan }: any) => {
-  let {
-    plan_id,
-    plan_lsTimes,
-    plan_authorizedTeams,
-    plan_authorizedUsers,
-    plan_createdAt,
-  } = plan;
+// const InviteLabel = ({ plan }: any) => {
+const InviteLabel = () => {
+  //   let {
+  //     plan_id,
+  //     plan_lsTimes,
+  //     plan_authorizedTeams,
+  //     plan_authorizedUsers,
+  //     plan_createdAt,
+  //   } = plan;
   return (
-    <div className='rounded-xl w-[25rem] h-[20rem] bg-gradient-to-r p-[5px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
-      <div className='flex flex-col items-center justify-center h-full bg-slate-800 text-white rounded-lg p-2'>
-        <span className='flex flex-col items-center w-full h-full rounded-xl'>
+    <div className='rounded-xl w-[25rem] h-[30rem] bg-gradient-to-r p-[3px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
+      <div className='flex flex-col h-full w-full bg-slate-800 text-white rounded-lg'>
+        <span className='flex flex-col items-center w-full h-fit pt-2'>
           <Player
-            src='https://assets10.lottiefiles.com/packages/lf20_9ti102vm.json'
-            className='player w-[90px] h-[90px] '
+            src='https://assets10.lottiefiles.com/packages/lf20_qwATcU.json'
+            className='player w-[110px] h-[110px] '
             autoplay
             loop
-            speed={0.4}
+            speed={0.8}
           />
-          <div className='w-full h-full flex items-center justify-center flex-col space-y-1 overflow-y-scroll'>
+        </span>
+        <div className='w-full h-full flex items-center justify-center flex-wrap content-center overflow-y-scroll overflow-x-hidden flex-col'>
+          <span className='flex items-center justify-center  flex-col'>
+            <HiOutlineKey className='text-[1.15rem] align-center justify-center' />
             <h1 className='text-blue-500 text-center font-Inter font-black text-lg'>
-              PLAN ID
+              PLAN ID:
             </h1>
-            <h1 className='text-blue-200 text-center text-lg font-bold '>
-              {plan_id}
+            <h1 className='text-blue-200 text-center text-lg font-bold pb-5'>
+              DUMMY PLAN ID
             </h1>
+          </span>
 
-            <h1 className='text-blue-500 text-center font-black text-lg'>
+          <span className='flex items-center justify-center  flex-col'>
+            <FaUserCircle className='text-[1.15rem] align-center justify-center' />
+            <h1 className='text-blue-500 text-center font-Inter font-black text-lg'>
+              INVITED BY:
+            </h1>
+            <h1 className='text-blue-200 text-center text-lg font-bold pb-5'>
+              DUMMY INVITED YOU
+            </h1>
+          </span>
+
+          <span className='flex items-center justify-center flex-col'>
+            <BsCalendar className='text-[1.075rem] align-center justify-center' />
+            <h1 className='text-blue-500 text-center font-Inter font-black text-lg'>
               PLAN CREATED:
             </h1>
             <h1 className='text-blue-200 text-center text-lg font-bold '>
-              {new Date(plan_createdAt).toUTCString()}
+              DUMMY PLAN CREATED
             </h1>
-            {plan_lsTimes.map((time: string) => {
-              return (
-                <h1
-                  className='text-blue-500 text-center font-black text-lg'
-                  key={uuidv4()}
-                >
-                  {time}
-                </h1>
-              );
-            })}
-            {plan_authorizedUsers.map((user: string) => {
-              return <pre className='text-white text-center'>{user}</pre>;
-            })}
-            {plan_authorizedTeams.map((user: string) => {
-              return <pre className='text-white text-center'>{user}</pre>;
-            })}
-          </div>
-          <div className='flex items-center justify-center'>
-            <Link href={`/dashboard/${plan_id}`}>
-              <button className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 w-[10rem] h-[3rem] overflow-hidden text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '>
-                <span className='relative px-5 py-2.5 transition-all ease-in duration-200 w-[9.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-full group-hover:bg-opacity-0'>
-                  View Plan
+          </span>
+        </div>
+        <div className='flex items-center justify-center w-full h-fit pb-4 space-x-4'>
+          <Link href='#'>
+            <button className='relative flex items-center justify-center  w-[5rem] h-[3rem] text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '>
+              <span className='relative px-5 py-2.5 group-hover:px-0 transition-all ease-in duration-200 w-[4.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-full group-hover:bg-opacity-0'>
+                <span className='flex items-center justify-around'>
+                  <FaCheck className='text-xl align-center justify-center relative top-[1px] group-hover:text-green-600' />
                 </span>
-              </button>
-            </Link>
-          </div>
-        </span>
+              </span>
+            </button>
+          </Link>
+
+          <button
+            // onClick={() => deleteCB(plan_id)}
+            className='group relative flex items-center justify-center  w-[5rem] h-[3rem] text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '
+          >
+            <span className='relative px-5 py-2.5 group-hover:px-0 transition-all ease-in duration-200 w-[4.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-full group-hover:bg-opacity-0'>
+              <span className='flex items-center justify-around'>
+                <ImCross
+                  title='Delete Plan'
+                  // onClick={() => deleteCB(plan_id)}
+                  className='cursor-pointer text-[1.3rem] align-center justify-center group-hover:text-red-700 transition-all duration-300'
+                />
+              </span>
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );

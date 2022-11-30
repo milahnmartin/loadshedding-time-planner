e2638d96dd77cd497bb611589c9b42940a02de0d
MrT1TAN <henri.louw.za@icloud.com>
2022-11-30 13:12:13 +0200

    Update (Invites Setup)

--- src/components/InviteMain.tsx
+++ src/components/InviteMain.tsx
@@ -1,7 +1,15 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import { NextPage } from "next";
+import Head from "next/head";
+
 const InviteMain = () => {
   return (
-    <div className='h-[90%] w-full border-2'>
-      <h1 className='text-white'>HELLO WORLD</h1>
+    <div className='h-screen w-screen overflow-y-scroll'>
+      <Head>
+        <title>LS Planner / Invites</title>
+      </Head>
+
+      <div className='flex min-h-[90vh] max-h-fit w-full flex-wrap content-center items-center justify-center overflow-y-scroll gap-2 py-4'></div>
     </div>
   );
 };

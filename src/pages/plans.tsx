12d9a74bf377521c44eaf7ffd75d47780a8388db
MrT1TAN <henri.louw.za@icloud.com>
2022-12-01 15:35:21 +0200

    Update (No Plan) : Button

--- src/pages/plans.tsx
+++ src/pages/plans.tsx
@@ -8,6 +8,7 @@ import { NextPage } from "next";
 import Head from "next/head";
 import { toast } from "react-toastify";
 import { v1 as uuidv1 } from "uuid";
+import Link from "next/link";
 
 const plans: NextPage = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -23,10 +24,7 @@ const plans: NextPage = () => {
       toast.error("No Plan Id Provided");
       return;
     }
-    const { error } = await supabase
-      .from("user_plans")
-      .delete()
-      .eq("plan_id", plan_id);
+    const { error } = await supabase.from("user_plans").delete().eq("plan_id", plan_id);
 
     if (error) {
       toast.error(error.message);
@@ -53,15 +51,13 @@ const plans: NextPage = () => {
           />
         ) : (
           savedPlans?.map((plan: any) => {
-            return (
-              <PlansLabel plan={plan} key={uuidv1()} deleteCB={handlePlanDelete} />
-            );
+            return <PlansLabel plan={plan} key={uuidv1()} deleteCB={handlePlanDelete} />;
           })
         )}
         {savedPlans?.length === 0 && (
           <div className='rounded-xl w-[20rem] h-fit bg-gradient-to-r p-[3px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
             <div className='flex flex-col h-full w-full bg-slate-800 text-white rounded-lg'>
-              <span className='flex flex-col items-center w-full h-fit pt-2'>
+              <span className='flex flex-col items-center w-full h-fit pt-2 pb-4'>
                 <Player
                   src='https://assets3.lottiefiles.com/private_files/lf30_17bvu2tk.json'
                   className='player w-[110px] h-[110px] '
@@ -70,6 +66,11 @@ const plans: NextPage = () => {
                   speed={0.5}
                 />
                 <h1 className='text-2xl font-bold pb-4'>NO PLANS</h1>
+                <button className='relative flex items-center justify-center  w-[10rem] h-[3rem] text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '>
+                  <span className='relative px-5 py-2.5 group-hover:px-0 transition-all ease-in duration-200 w-[9.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-full group-hover:bg-opacity-0'>
+                    <span className='flex items-center justify-around'>Create Plan</span>
+                  </span>
+                </button>
               </span>
             </div>
           </div>

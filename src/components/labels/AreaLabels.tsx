67859e6c7fac1ee0518f558900fb22d238797c72
MrT1TAN <henri.louw.za@icloud.com>
2022-12-01 16:01:57 +0200

    Update (Area Labels) : bg

--- src/components/labels/AreaLabels.tsx
+++ src/components/labels/AreaLabels.tsx
@@ -45,9 +45,9 @@ const AreaLabels = ({ id, name, region, cbSetArea }: AreaLabelsProps) => {
 
           <button
             onClick={() => cbSetArea({ id, name, region })}
-            className='relative inline-flex items-center justify-center p-0.5  w-[10rem] h-[3rem] overflow-hidden text-sm font-medium text-gray-900 rounded-[1.15rem] group bg-gradient-to-br from-c2aqua via-c2blue to-c2purple group-hover:from-c2aqua group-hover:via-c2blue group-hover:to-c2purple hover:text-white dark:text-white '
+            className='relative inline-flex items-center justify-center p-0.5  w-[10rem] h-[3rem] overflow-hidden text-sm font-medium text-slate-800 rounded-[1.15rem] group bg-gradient-to-br from-c2aqua via-c2blue to-c2purple group-hover:from-c2aqua group-hover:via-c2blue group-hover:to-c2purple hover:text-white dark:text-white '
           >
-            <span className='relative px-5 py-2.5 transition-all ease-in duration-200  w-[9.5rem] h-[2.5rem] bg-white dark:bg-gray-900 rounded-[1rem] group-hover:bg-opacity-0 font-bold'>
+            <span className='relative px-5 py-2.5 transition-all ease-in duration-200  w-[9.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-[1rem] group-hover:bg-opacity-0 font-bold'>
               SET AREA
             </span>
           </button>

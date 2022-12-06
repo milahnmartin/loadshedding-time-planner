5ee6072a729d84b11255ecbd99b7a2d32a0fbff3
MrT1TAN <henri.louw.za@icloud.com>
2022-12-06 21:36:30 +0200

    Update (Team Settings) : Lottie

--- src/components/profile/TeamProfile.tsx
+++ src/components/profile/TeamProfile.tsx
@@ -9,12 +9,12 @@ const TeamProfile = () => {
         <div className='rounded-xl w-[45rem] h-fit bg-gradient-to-r p-[3px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
           <div className='flex flex-col h-full w-full bg-slate-800 text-white rounded-lg'>
             <span className='flex flex-col items-center w-full h-fit pt-4 pb-4'>
-              <h1 className='text-3xl font-satoshiBlack pt-6 pb-4'>
+              <h1 className='text-3xl font-satoshiBlack pt-4 pb-6'>
                 TEAM SETTINGS COMMING SOON
               </h1>
               <Player
                 src='https://assets4.lottiefiles.com/packages/lf20_oo3N9WVAgU.json'
-                className='player w-[300px] h-[300px] '
+                className='player w-[350px] h-[350px] '
                 autoplay
                 loop
                 speed={0.5}

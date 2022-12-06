2fbb04f0a2e999093659cca55254684a9921bb3c
MrT1TAN <henri.louw.za@icloud.com>
2022-12-06 20:46:56 +0200

    Update (TeamSettings) : Card

--- src/components/profile/TeamProfile.tsx
+++ src/components/profile/TeamProfile.tsx
@@ -8,7 +8,7 @@ const TeamProfile = () => {
       <div className='w-full flex flex-col items-center justify-center'>
         <div className='rounded-xl w-[45rem] h-fit bg-gradient-to-r p-[3px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
           <div className='flex flex-col h-full w-full bg-slate-800 text-white rounded-lg'>
-            <span className='flex flex-col items-center w-full h-fit pt-2 pb-4'>
+            <span className='flex flex-col items-center w-full h-fit pt-4 pb-4'>
               <Player
                 src='https://assets4.lottiefiles.com/packages/lf20_oo3N9WVAgU.json'
                 className='player w-[300px] h-[300px] '
@@ -16,8 +16,8 @@ const TeamProfile = () => {
                 loop
                 speed={0.5}
               />
-              <h1 className='text-2xl font-satoshiBlack pb-4'>
-                TEAM INVITES AND SETTINGS COMMING SOON
+              <h1 className='text-3xl font-satoshiBlack pt-6 pb-4'>
+                TEAM SETTINGS COMMING SOON
               </h1>
             </span>
           </div>

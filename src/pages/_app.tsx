34569f6c93af3eac05a4df3e6e253e2660dc1ee8
milahnmartin <milahnmartin.develop@gmail.com>
2022-12-08 23:27:48 +0200

    feat(backgroundMesh):Added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,34 @@
+import { Gradient } from "@helpers/Gradient.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppType } from "next/dist/shared/lib/utils";
+import { useEffect, useRef } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
 import "../styles/satoshi.css";
+const gradient = new Gradient() as any;
 const MyApp: AppType = ({ Component, pageProps }) => {
   const queryClient = new QueryClient();
 
+  const ref = useRef();
+  useEffect(() => {
+    if (ref.current) {
+      console.log(ref);
+      gradient.initGradient("#gradient-canvas");
+    }
+  }, [ref]);
+
   return (
-    <div className='bg-gradient-to-b from-gray-700 via-gray-900 to-black'>
-      <QueryClientProvider client={queryClient}>
-        <ToastContainer position='bottom-right' />
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </div>
+    <canvas className='w-full h-[90%]' id='gradient-canvas' data-transition-in>
+      <div ref={ref} className='ref'>
+        <QueryClientProvider client={queryClient}>
+          <ToastContainer position='bottom-right' />
+          <Component ref={ref} {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </div>
+    </canvas>
   );
 };
 

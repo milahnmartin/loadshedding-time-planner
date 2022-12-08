de40a614b53dde0c23e0c53460ee47c480367317
milahnmartin <milahnmartin.develop@gmail.com>
2022-12-08 18:22:13 +0200

    fix(fonts):Preload StylesSheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,31 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
-
+import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 export default class MyDocument extends Document {
-  static async getIntitialProps(ctx: any) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+  static async getIntitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
   }
   render() {
     return (
       <Html>
         <Head>
           <link rel='icon' href='/Logov3.png' />
-          <link rel='stylesheet' href='../styles/satoshi.css' />
-          <link rel='stylesheet' href='../styles/globals.css' />
+          <link rel='stylesheet' href='/src/styles/satoshi.css' />
+          <link rel='stylesheet' href='/src/styles/globals.css' />
         </Head>
         <body>
           <Main />

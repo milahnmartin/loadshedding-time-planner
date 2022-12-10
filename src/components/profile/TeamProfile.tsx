9bdc6b1e9a1f550d240d34e0f20d8d515e2930a3
MrT1TAN <henri.louw.za@icloud.com>
2022-12-10 11:34:56 +0200

    Update (Team Settings Card) : Border

--- src/components/profile/TeamProfile.tsx
+++ src/components/profile/TeamProfile.tsx
@@ -6,7 +6,7 @@ const TeamProfile = () => {
   return (
     <div className='p-5 w-full h-full flex items-center '>
       <div className='w-full flex flex-col items-center justify-center'>
-        <div className='rounded-xl w-[45rem] h-fit bg-gradient-to-r p-[3px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
+        <div className='rounded-xl w-[45rem] h-fit bg-gradient-to-r p-[4px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
           <div className='flex flex-col h-full w-full bg-slate-800 text-white rounded-lg'>
             <span className='flex flex-col items-center w-full h-fit pt-4 pb-4'>
               <h1 className='text-3xl font-satoshiBlack pt-4 pb-6'>

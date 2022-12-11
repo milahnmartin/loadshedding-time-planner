d8777f841420a2efa1a526709e8839d7c21ba4b9
milahnmartin <milahnmartin.develop@gmail.com>
2022-12-11 15:32:43 +0200

    feat(dashboard):Redirect If Unauthorized

--- src/hooks/useFetchPlanData.tsx
+++ src/hooks/useFetchPlanData.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
+import { auth } from "@utils/firebase-config";
 import Router from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { toast } from "react-toastify";
 import supabase from "../utils/supabase-config";
 const fetchPlanData = async ({ queryKey }: any) => {
@@ -10,19 +12,34 @@ const fetchPlanData = async ({ queryKey }: any) => {
       `plan_lsTimes,plan_authorizedUsers,plan_InvitedData,user_id,plan_authorizedTeams,plan_createdAt`
     )
     .eq("plan_id", queryKey[1]);
+
   if (error || !data || data.length === 0) {
     toast.error("Plan not found, redirecting to plans...");
     Router.push("/plans");
     return null;
   }
 
+  const { user_id, plan_authorizedUsers }: any = data[0];
+
+  if (user_id !== queryKey[2] && !plan_authorizedUsers.includes(queryKey[2])) {
+    toast.error("You are not authorized to view this plan");
+    Router.push("/plans");
+  }
+
   return data[0];
 };
 
 export default function useFetchPlanData(planID: string) {
-  return useQuery(["specificPlan", planID], fetchPlanData, {
+  const [user, loading] = useAuthState(auth);
+  if (!loading) {
+    if (!user) {
+      Router.push("/auth/login");
+      toast.warning("You need to be logged in to view this page");
+    }
+  }
+  return useQuery(["specificPlan", planID, user?.uid], fetchPlanData, {
     refetchOnWindowFocus: false,
     staleTime: Infinity,
-    enabled: !!planID,
+    enabled: !!planID && !!user?.uid,
   });
 }

31d99bd8c5c1ff9f5ef26daea53527a47acee607
MrT1TAN <henri.louw.za@icloud.com>
2022-12-13 18:16:51 +0200

    Update (Filter) : Input Styling

--- src/components/plan/PlanFilter.tsx
+++ src/components/plan/PlanFilter.tsx
@@ -253,7 +253,17 @@ function PlanFilter({
               ref={inviteInputRef}
             />
           </div>
-          <button onClick={handleInviteMember}>SEND INVITE</button>
+          {/* <button onClick={handleInviteMember}>SEND INVITE</button> */}
+          <button
+            className='relative flex items-center justify-center  w-[23rem] h-[3rem] text-sm font-black text-gray-900 rounded-xl group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '
+            onClick={handleInviteMember}
+          >
+            <span className='relative px-5 py-2.5 group-hover:px-0 transition-all ease-in duration-200 w-[22.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-lg group-hover:bg-opacity-0'>
+              <span className='flex items-center justify-around font-satoshiBlack'>
+                SEND INVITE
+              </span>
+            </span>
+          </button>
           <div className='text-base rounded-xl w-full h-fit bg-gradient-to-r p-[4px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]'>
             <input
               type='text'
@@ -261,7 +271,17 @@ function PlanFilter({
               placeholder='Invite Team Via ID or Name'
             />
           </div>
-          <button>INVITE TEAM</button>
+          {/* <button>INVITE TEAM</button> */}
+          <button
+            className='relative flex items-center justify-center  w-[23rem] h-[3rem] text-sm font-black text-gray-900 rounded-xl group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white '
+            onClick={handleInviteMember}
+          >
+            <span className='relative px-5 py-2.5 group-hover:px-0 transition-all ease-in duration-200 w-[22.5rem] h-[2.5rem] bg-white dark:bg-slate-800 rounded-lg group-hover:bg-opacity-0'>
+              <span className='flex items-center justify-around font-satoshiBlack'>
+                INVITE TEAM
+              </span>
+            </span>
+          </button>
         </div>
 
         <div className='border-2 w-full h-full overflow-y-scroll justify-start flex flex-col p-2'>

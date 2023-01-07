f7a0af34b19344db6476ad3b7293f737737430a5
milahnmartin <66036885+milahnmartin@users.noreply.github.com>
2023-01-07 21:19:43 +0200

    feat(404):New 404 Anim

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,9 +1,9 @@
-import Logo from "@assets/Logov3.png";
+import ErrorJSON from "@assets/119776-table-fan-404-error.json";
 import Footer from "@comps/ui/Footer";
 import Navbar from "@comps/ui/Navbar";
 import Head from "next/head";
-import Image from "next/image";
 import Router from "next/router";
+import Lottie from "react-lottie-player";
 const ErrorPage = () => {
   return (
     <div className='h-screen w-screen overflow-hidden'>
@@ -11,23 +11,8 @@ const ErrorPage = () => {
         <title>LS Planner / 404</title>
       </Head>
       <Navbar />
-      <div className='h-[90%] w-ful flex-col space-y-14 flex items-center justify-center'>
-        <Image
-          className='animate-bounce'
-          src={Logo}
-          alt='Logo'
-          width={200}
-          height={200}
-        />
-        <h1 className='text-transparent bg-clip-text bg-gradient-to-r from-cblue via-cpurple to-caqua font-satoshiBold text-6xl font-black'>
-          THIS PAGE DOES NOT EXIST
-        </h1>
-        <button
-          onClick={() => Router.push("/")}
-          className='hover:bg-cpurple px-4 py-2 animation-all duration-200 text-white font-satoshiBold bg-cblue rounded-xl'
-        >
-          RETURN HOME
-        </button>
+      <div className='h-[90%] w-full flex-col flex items-center justify-center'>
+        <Lottie loop animationData={ErrorJSON} play style={{ width: 400, height: 400 }} />
       </div>
       <Footer />
     </div>

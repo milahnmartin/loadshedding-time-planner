ba0db6d50f4de30dc3a32444a10f7b82f5b19b59
MrT1TAN <henri.louw.za@icloud.com>
2023-01-14 17:34:18 +0200

    Update (Docs) : Intro

--- src/components/docs/Introduction.tsx
+++ src/components/docs/Introduction.tsx
@@ -10,7 +10,11 @@ function Introduction() {
         plan. Other users then accept the invite and get added to the "plan". LS PLANNER
         then compares all the users on the "plan's" loadshedding times and then shows all
         available times where nobody on the "plan" has loadshedding in the chosen
-        timeframe.
+        timeframe. Why use LS PLANNER? There is nothing more frustrating than trying to do
+        something productive or playing some games with friends just for loadshedding to
+        end it. It is predicted that loadshedding will continue till 2027 if not longer.
+        LS PLANNER can't do anything about that, but we can make it easier for you to
+        schedule meetings and gaming sessions with friends :)
       </h1>
     </div>
   );

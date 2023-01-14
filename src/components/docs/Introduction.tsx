096217aaf54d8f29b26a158b5820af61b8c99dfe
MrT1TAN <henri.louw.za@icloud.com>
2023-01-14 17:26:26 +0200

    Update (Docs) : Intro

--- src/components/docs/Introduction.tsx
+++ src/components/docs/Introduction.tsx
@@ -1,7 +1,17 @@
 function Introduction() {
   return (
     <div>
-      <h1>INTRODUCTION</h1>
+      <h1>
+        What is LS PLANNER? LS PLANNER is a tool that makes it simple and easy to schedule
+        meetings and gaming sessions around loadshedding. How does it work? Users set
+        their loadshedding location. LS PLANNER then automatically tracks their
+        loadshedding schedule through an API. The user then creates a "plan" where they
+        can choose a date, start and end time and invite other users or teams to their
+        plan. Other users then accept the invite and get added to the "plan". LS PLANNER
+        then compares all the users on the "plan's" loadshedding times and then shows all
+        available times where nobody on the "plan" has loadshedding in the chosen
+        timeframe.
+      </h1>
     </div>
   );
 }

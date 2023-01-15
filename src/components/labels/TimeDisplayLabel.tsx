ef4420380bceead1faf8a3c5fcb31e827ce87b48
milahnmartin <milahnmartin.develop@gmail.com>
2023-01-15 18:31:11 +0200

    fix(TimeLabel):Refac Html Element

--- src/components/labels/TimeDisplayLabel.tsx
+++ src/components/labels/TimeDisplayLabel.tsx
@@ -7,20 +7,13 @@ type LabelProps = {
 };
 
 const GreenLabel = ({ data, variant }: LabelProps) => {
-  const lscolorClassName = classNames(
-    "text-white text-1xl rounded-lg flex items-center justify-center px-4 py-2 self-center",
-    {
-      "bg-red-700": variant === "ls",
-      "bg-cblue": variant === "availible",
-      "bg-yellow-500": variant === "buffer",
-    }
-  );
+  const lscolorClassName = classNames("text-white text-1xl rounded-lg px-4 py-2", {
+    "bg-red-700": variant === "ls",
+    "bg-cblue": variant === "availible",
+    "bg-yellow-500": variant === "buffer",
+  });
 
-  return (
-    <div className={lscolorClassName}>
-      <h1 className='font-satoshiBold'>{data}</h1>
-    </div>
-  );
+  return <span className={lscolorClassName}>{data}</span>;
 };
 
 export default GreenLabel;

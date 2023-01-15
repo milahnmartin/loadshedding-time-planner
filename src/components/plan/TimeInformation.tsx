5714377bd09e389a3690a7b4aff77c687d886a17
milahnmartin <milahnmartin.develop@gmail.com>
2023-01-15 18:40:03 +0200

    feat(TimeInformation):Added Info

--- src/components/plan/TimeInformation.tsx
+++ src/components/plan/TimeInformation.tsx
@@ -2,6 +2,7 @@ import TimeDisplayLabel from "@comps/labels/TimeDisplayLabel";
 import TimeCalc from "@helpers/algorithm";
 import useFetchLoadsheddingStatus from "@hooks/useFetchLoadsheddingStatus";
 import { useEffect, useState } from "react";
+import { CiCircleInfo } from "react-icons/ci";
 type Times = {
   bufferTimes: any[];
   availableTimes: any[];
@@ -37,8 +38,15 @@ function TimeInformation({ LSTimes, timeScope }: Props) {
   return (
     <div className='h-full w-6/12 border-2 flex-col text-white font-satoshi border-red-600 flex items-center justify-center flex-wrap content-start overflow-y-scroll'>
       <div className='w-full h-1/3 border-2'>
-        <div className='flex w-full items-center justify-center text-center h-[20%]'>
-          <pre>AVAILABLE TIMES</pre>
+        <div className='flex w-full items-center justify-center text-center h-[20%] group'>
+          <span className='flex items-center justify-center gap-2'>
+            <pre>AVAILABLE TIMES</pre>
+            <CiCircleInfo
+              title='Available Times Show times where all users are not experiencing on loadshedding'
+              className='hover:text-cblue transition-colors duration-500  cursor-pointer group-hover:animate-wiggle'
+              size={25}
+            />
+          </span>
         </div>
         <div className='flex h-[80%] w-full border-2 items-center justify-center gap-2 flex-wrap content-center'>
           {data &&
@@ -48,8 +56,15 @@ function TimeInformation({ LSTimes, timeScope }: Props) {
         </div>
       </div>
       <div className='w-full h-1/3 border-2'>
-        <div className='flex w-full items-center justify-center text-center h-[20%]'>
-          <pre>BUFFER TIMES</pre>
+        <div className='flex w-full items-center justify-center text-center h-[20%] group'>
+          <span className='flex items-center justify-center gap-2'>
+            <pre>BUFFER TIMES</pre>
+            <CiCircleInfo
+              title='Buffer Times add 30 min before and after each available time'
+              className='hover:text-yellow-500 transition-colors duration-500  cursor-pointer group-hover:animate-wiggle'
+              size={25}
+            />
+          </span>
         </div>
         <div className='flex h-[80%] w-full border-2 items-center justify-center gap-2 flex-wrap content-center'>
           {data &&
@@ -59,8 +74,15 @@ function TimeInformation({ LSTimes, timeScope }: Props) {
         </div>
       </div>
       <div className='w-full h-1/3 border-2'>
-        <div className='flex w-full items-center justify-center text-center h-[20%]'>
-          <pre>LS TIMES</pre>
+        <div className='flex w-full items-center justify-center text-center h-[20%] group'>
+          <span className='flex items-center justify-center gap-2'>
+            <pre>LS TIMES</pre>
+            <CiCircleInfo
+              title='All Users Active Loadshedding Times'
+              className='hover:text-red-700 transition-colors duration-500  cursor-pointer group-hover:animate-wiggle'
+              size={25}
+            />
+          </span>
         </div>
         <div className='flex h-[80%] w-full border-2 items-center justify-center gap-2 flex-wrap content-center'>
           {data &&

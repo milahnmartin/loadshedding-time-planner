d35384a793e0d11fccd4d04de6810ab0d804f044
MrT1TAN <henri.louw.za@icloud.com>
2023-01-16 21:46:33 +0200

    Update (Docs) : HTU

--- src/components/docs/HowToUse.tsx
+++ src/components/docs/HowToUse.tsx
@@ -1,12 +1,41 @@
 import { Player } from "@lottiefiles/react-lottie-player";
+import { useQueryClient } from "@tanstack/react-query";
 import classNames from "classnames";
-
+import { toast } from "react-toastify";
+import Router from "next/router";
+import { v4 as uuidv4 } from "uuid";
+import supabase from "@utils/supabase-config";
+import { auth } from "@utils/firebase-config";
+import { IoIosArrowForward, IoMdArrowRoundForward } from "react-icons/io";
+import { useAuthState } from "react-firebase-hooks/auth";
 function HowToUse() {
+  const queryClient = useQueryClient();
+  const [user, loading] = useAuthState(auth);
+  const handleNewPlan = async () => {
+    if (!user) {
+      Router.push("/auth/login");
+      toast.error("You Need To be Logged In To Create A Plan");
+      return;
+    }
+    const newPlanUUID = uuidv4();
+    const { error } = await supabase.from("user_plans").insert({
+      plan_id: newPlanUUID,
+      user_id: user?.uid,
+      plan_createdAt: new Date().toISOString(),
+    });
+    if (error) {
+      console.log(error);
+      toast.error("Error Creating Plan");
+      return;
+    }
+    queryClient.invalidateQueries({ queryKey: ["savedplans"] });
+    Router.push(`/dashboard/${newPlanUUID}`);
+  };
   const gline = classNames(
     " w-[33%] h-[0.25rem] bg-gradient-to-r from-[#6EE7B7] via-[#3B82F6] to-[#9333EA] rounded border-0 "
   );
   return (
-    <div className='flex flex-col h-full w-full pl-8 pr-8'>
+    <div className='flex flex-col overflow-y-scroll h-full w-full pl-8 pr-8'>
       <span className='flex flex-col items-center w-full h-fit pb-10'>
         <Player
           src='https://assets1.lottiefiles.com/packages/lf20_fifomona.json'
@@ -29,15 +58,40 @@ function HowToUse() {
         />
       </span>
       <p className='text-white font-satoshi text-lg pb-4'>
-        Follow the steps below to start using LS PLANNER
+        The following steps will show you how to use LS PLANNER
       </p>
       <hr className={gline} />
-      <h2 className='text-white font-satoshi text-lg pt-2'>
-        1.&ensp;Navigate to the{" "}
-        <a className='underline font-satoshiBold' href='/auth/login'>
-          SIGN IN
-        </a>{" "}
-        button on the top right of the screen
+
+      <h2 className='flex text-white font-satoshi text-lg pt-4  '>
+        1.&ensp;Navigate to the {"  "}&ensp;
+        <button
+          onClick={handleNewPlan}
+          className='relative inline-flex font-satoshiBold items-center justify-evenly p-0.5 mr-2 w-[8rem] h-[2rem] overflow-hidden text-sm font-black text-gray-900 rounded-full group bg-gradient-to-br from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]  hover:text-white dark:text-white '
+        >
+          New Plan
+          <div className='group h-full flex items-center'>
+            <IoMdArrowRoundForward
+              id='my-arrow'
+              size={20}
+              className='hidden group-hover:inline transition-all duration-250'
+            />
+            <IoIosArrowForward
+              id='normal-arrow'
+              size={20}
+              className='group-hover:hidden transition-all duration-250'
+            />
+          </div>
+        </button>{" "}
+        button on the home page
+      </h2>
+
+      <h2 className='text-white font-satoshi text-lg'>
+        2. When clicking the button, you will be redirected to a dashboard and a "LS plan"
+        will be created.
+      </h2>
+      <h2 className='text-white font-satoshi text-lg'>
+        3. You will see a filter slide in from the right, where you can customize or
+        invite other users to your "LS plan" settings
       </h2>
     </div>
   );
